80f0d99c0b36f6cb233fccb045d6888b7ffc1050
sugano <sugaaaano@gmail.com>
2023-06-30 11:28:21 +0900

    fix: link
    close #66

--- components/molecules/LinkMenuColumn.vue
+++ components/molecules/LinkMenuColumn.vue
@@ -2,7 +2,10 @@
     <div class="link-menu-column">
         <p class="text-link"><a :class="{ dark: dark }" @click="scrollTo">{{ heading }}</a></p>
         <p v-for="item of contents" class="text-link">
-            <NuxtLink :class="{ dark: dark }" :to="item.url">
+            <a v-if="isCurrentUrl(item.url)" :class="{ dark: dark }" @click="closeModalsAndScrollToTop">
+                {{ item.label }}
+            </a>
+            <NuxtLink v-else :class="{ dark: dark }" :to="item.url">
                 {{ item.label }}
             </NuxtLink>
         </p>
@@ -10,6 +13,26 @@
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+const showProfileModal = useState('showProfileModal')
+const showModal = useState('showModal')
+const dark = useState('dark')
+const darkGrad = useState('darkGrad')
+const smoother = useState<ScrollSmoother>('smoother')
+
+function isCurrentUrl(url: string): boolean
+{
+    return route.path === url;
+}
+function closeModalsAndScrollToTop(): void
+{
+    showProfileModal.value = false;
+    showModal.value = false;
+    dark.value = false;
+    darkGrad.value = false;
+    smoother.value.scrollTop(0);
+}
+
 interface Item
 {
     label: string;
